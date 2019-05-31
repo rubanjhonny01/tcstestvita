311fd106a056a2ecc9d94f7b2a90dfc2847e25b9
rubanjhonny01 <50738042+rubanjhonny01@users.noreply.github.com>
2019-05-31 20:55:57 +0530

    Create primerange.py

---
+++ primerange.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+for i in range(n+1,k):
+  count1=0
+  for j in range(1,i):
+    if i%j==0:
+      count1+=1
+  if count1==1:
+    l.append(i)
+for x in l:
+  print(x,end="")
